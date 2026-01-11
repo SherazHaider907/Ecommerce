4ae0bdc7634aaa56fd3e85b15592bb8bf6ca0583
SherazHaider907 <Sherazhaider665@gmail.com>
2026-01-11 20:40:53 +0500

    create store app and Updated product in models and admin models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'is_available', 'category', 'created_date')
+    list_filter = ('is_available', 'category')
+    search_fields = ('product_name', 'description')
+    prepopulated_fields = {'slug': ('product_name',)}
+
+
+admin.site.register(Product, ProductAdmin)
