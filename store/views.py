2570fc75a55b54b49ea73874a2c9533e76a71bed
SherazHaider907 <Sherazhaider665@gmail.com>
2026-01-13 23:47:26 +0500

    add url links to images and products

--- store/views.py
+++ store/views.py
@@ -24,6 +24,6 @@ def product_detail(request,category_slug,product_slug ):
     except Exception as e:
         raise e
     context = {
-        'single_product':single_product,
+        'single_product':single_product, 
     }
     return render(request,'store/product_detail.html',context)
