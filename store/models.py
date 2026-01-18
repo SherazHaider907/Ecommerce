e2af7576bd25fe729b3a0e51de2901f1ff776471
SherazHaider907 <Sherazhaider665@gmail.com>
2026-01-18 23:42:35 +0500

    n

--- store/models.py
+++ store/models.py
@@ -42,6 +42,5 @@ class Variation(models.Model):
     created_date       = models.DateTimeField(auto_now=True)
 
     objects = VariationManagar()
-    print("hi")
     def __unicode__(self):
         return self.product
