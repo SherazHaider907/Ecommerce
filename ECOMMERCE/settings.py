532156fe6519bf1e23a29351eb5c65698cb576d8
SherazHaider907 <Sherazhaider665@gmail.com>
2026-01-22 17:42:16 +0500

    fix email

--- ECOMMERCE/settings.py
+++ ECOMMERCE/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sherazhaider665@gmail.com'
-EMAIL_HOST_PASSWORD = '75397561az'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
+    path('dashboard/',views.dashboard,name='dashboard'),
     # path('cart/',views.cart,name='cart'),
     # path('add_cart/<int:product_id>/',views.add_cart, name='add_cart'),
     # path('remove_cart/<int:product_id>/<int:cart_item_id>/',views.remove_cart, name='remove_cart'),
