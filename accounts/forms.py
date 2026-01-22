2a1ccfb1a5e4ff4e83ce4eaad4eb5badee13ca3a
SherazHaider907 <Sherazhaider665@gmail.com>
2026-01-22 12:49:53 +0500

    registrations complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,7 +11,7 @@ class RegistrationForm(forms.ModelForm):
     }))
     class Meta:
         model = Account
-        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password', 'confirm_password']
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
@@ -22,3 +22,14 @@ class RegistrationForm(forms.ModelForm):
 
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+    
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password and Confirm Password does not match"
+            )
+        return cleaned_data
