98a56a5e0b3a66d90e9d09bac7e55bb157d6dd7a
SherazHaider907 <Sherazhaider665@gmail.com>
2026-01-27 23:55:12 +0500

    making an order in dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+
+    # dashboard orders
+    path('my_orders/', views.my_orders, name='my_orders'),   
 ]
